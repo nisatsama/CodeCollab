ddcae49e15bb163b67f3d47c0583e1e6d56baeb0
nisatsama <nisatsama7547@gmail.com>
2026-06-19 19:19:24 +0530

    Added vercel link for frontend

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://code-collab-phi-one.vercel.app/",
+    ],
     methods: ["GET", "POST"],
   },
 });
